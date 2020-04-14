49ca2bb288779b181b1c32482c0bd780946274c2
Shoaib Mohammad Ankon <shoaib.ankon@gmail.com>
2020-04-14 19:45:46 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask,render_template, request, redirect, url_for 
 from flask_mysqldb import MySQL
 
+
 app = Flask(__name__) 
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -44,7 +45,40 @@ def insert():
 		mysql.connection.commit()
 	return redirect(url_for('read'))
 
-			
+
+@app.route('/delete/<string:id_data>', methods = ['GET'])
+def delete(id_data):    
+    cur = mysql.connection.cursor()
+    cur.execute(" DELETE FROM incident WHERE id=%s", (id_data,))
+    mysql.connection.commit()
+    return redirect(url_for('read'))
+
+
+@app.route('/update',methods=['POST','GET'])
+def update():
+
+    if request.method == 'POST':
+    	id_data = request.form['id']	
+    	issue = request.form['issue']
+    	impact = request.form['impact']
+    	status = request.form['status']
+    	startt = request.form['startt']
+    	endt = request.form['endt']
+    	reason = request.form['reason']
+    	solution = request.form['solution']
+    	ticket = request.form['ticket']
+    	remarks = request.form['remarks']
+    	cur = mysql.connection.cursor()
+    	cur.execute("""
+               UPDATE incident 
+               SET issue=%s, impact=%s, status=%s, startt=%s, endt=%s, reason=%s, solution=%s, ticket=%s, remarks=%s 
+               WHERE id=%s
+            """,(issue,impact,status,startt,endt,reason,solution,ticket,remarks,id_data))
+
+    	mysql.connection.commit()
+    return redirect(url_for('read'))
+
+
 
 
 if __name__ == '__main__': 
